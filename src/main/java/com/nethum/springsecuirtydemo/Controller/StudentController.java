a4c00436c51dff90daad34dff7db9ac3bcb01e2f
Nethum Mihiranga <157717066+Nethum99@users.noreply.github.com>
2025-03-04 10:20:38 +0530

    feat: Add CSRF Token handler
    Add a method to get CSRF token as Object to add new student.

--- src/main/java/com/nethum/springsecuirtydemo/Controller/StudentController.java
+++ src/main/java/com/nethum/springsecuirtydemo/Controller/StudentController.java
@@ -1,6 +1,10 @@
 package com.nethum.springsecuirtydemo.Controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -19,5 +23,13 @@ public class StudentController {
         return students;
     }
 
-    publ
+    @PostMapping("add_student")
+    public void addStudent(@RequestBody Student student){
+        students.add(student);
+    }
+
+    @GetMapping("csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest httpServletRequest){
+        return (CsrfToken) httpServletRequest.getAttribute("_csrf");
+    }
 }
