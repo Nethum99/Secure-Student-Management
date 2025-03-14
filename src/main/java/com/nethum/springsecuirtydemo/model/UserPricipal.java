5c3bfd183325ddfe50168c4cc85008798e36f5b0
Nethum Mihiranga <157717066+Nethum99@users.noreply.github.com>
2025-03-14 12:27:54 +0530

    Add UserPricipal to implement multiuser Authentication.

--- src/main/java/com/nethum/springsecuirtydemo/model/UserPricipal.java
+++ src/main/java/com/nethum/springsecuirtydemo/model/UserPricipal.java
@@ -1,4 +1,53 @@
 package com.nethum.springsecuirtydemo.model;
 
-public class UserPricipal {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class UserPricipal implements UserDetails {
+
+    private User user;
+
+    public UserPricipal(User user){
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
