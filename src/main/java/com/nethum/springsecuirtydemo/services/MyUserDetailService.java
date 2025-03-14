676d37eb588f43df8c7d28ac7d62b56f34ce3f43
Nethum Mihiranga <157717066+Nethum99@users.noreply.github.com>
2025-03-14 12:33:12 +0530

    Add service to verify if user exist or not

--- src/main/java/com/nethum/springsecuirtydemo/services/MyUserDetailService.java
+++ src/main/java/com/nethum/springsecuirtydemo/services/MyUserDetailService.java
@@ -1,4 +1,29 @@
 package com.nethum.springsecuirtydemo.services;
 
-public class MyUserDetailService {
+import com.nethum.springsecuirtydemo.model.User;
+import com.nethum.springsecuirtydemo.model.UserPricipal;
+import com.nethum.springsecuirtydemo.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUserName(username);
+
+        if(user == null){
+            System.out.println("User 404");
+            throw new UsernameNotFoundException("User 404");
+        }
+
+        return  new UserPricipal(user);
+    }
 }
