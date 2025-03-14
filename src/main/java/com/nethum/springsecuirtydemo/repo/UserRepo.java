ff67fb591cf9b4dd591312311b4be485dfd43f56
Nethum Mihiranga <157717066+Nethum99@users.noreply.github.com>
2025-03-14 12:29:12 +0530

    Add Repository layer to get username and password from database

--- src/main/java/com/nethum/springsecuirtydemo/repo/UserRepo.java
+++ src/main/java/com/nethum/springsecuirtydemo/repo/UserRepo.java
@@ -1,4 +1,10 @@
 package com.nethum.springsecuirtydemo.repo;
 
-public interface UserRepo {
+import com.nethum.springsecuirtydemo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
+    User findByUserName(String userName);
 }
