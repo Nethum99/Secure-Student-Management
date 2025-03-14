798b2849bcdec2b7072b36e10c1e419e19186819
Nethum Mihiranga <157717066+Nethum99@users.noreply.github.com>
2025-03-14 18:00:18 +0530

    Add BCrypt Encoder and Authenticator

--- src/main/java/com/nethum/springsecuirtydemo/services/UserService.java
+++ src/main/java/com/nethum/springsecuirtydemo/services/UserService.java
@@ -3,6 +3,7 @@ package com.nethum.springsecuirtydemo.services;
 import com.nethum.springsecuirtydemo.model.User;
 import com.nethum.springsecuirtydemo.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,13 @@ public class UserService {
     @Autowired
     private UserRepo userRepo;
 
+    private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(12);
+
     public User saveUser(User user){
+
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        System.out.println(user.getPassword());
         return userRepo.save(user);
+
     }
 }
